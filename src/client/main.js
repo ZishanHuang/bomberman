e651c286f39c67d70870caee4412d99b23116ef1
ZishanHuang <13242335+ZishanHuang@users.noreply.github.com>
2021-09-07 00:49:06 -0400

    fix: heroku dynamic port issue

--- src/client/main.js
+++ src/client/main.js
@@ -5,15 +5,16 @@ import SocketIO from 'socket.io-client'
 // import {socket} from './services/socket.js'
 // import VueSocketIOExt from 'vue-socket.io-extended';
 
-const host = process.env.HOST || 'http://localhost';
-const port = process.env.PORT || 8080;
-const socketUrl = `${host}:${port}`;
-console.log(socketUrl);
+// const host = process.env.HOST || 'http://localhost';
+// const port = process.env.PORT || 8080;
+// const socketUrl = `${host}:${port}`;
+// console.log(socketUrl);
 
 Vue.config.productionTip = false
 Vue.use(new VueSocketIO({
   // connection: socket,
-  connection: SocketIO(socketUrl),
+  // connection: SocketIO(socketUrl),
+  connection: SocketIO(),
   debug: true
 }))
 // Vue.use(VueSocketIOExt, SocketIO(socketUrl));
